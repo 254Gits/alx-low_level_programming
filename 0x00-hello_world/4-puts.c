958a971013b6ca967104b1b8ee9b0f54bae40a62
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-16 17:17:06 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char Myprint[] = "Programming is like building a multilingual puzzle\n";
+
+	puts(Myprint);
+
+	return (0);
+}
