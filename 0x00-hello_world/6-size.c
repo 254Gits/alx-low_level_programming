c9640f290db5f630a14a517e8102fd26b8e6bf7e
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-16 18:58:11 -0700

    addition of 32/64 bit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ int main(void)
 
 	printf("Size of a char:%lu byte(s)\n", sizeof(c));
 	printf("Size of an int:%lu byte(s)\n", sizeof(i));
-	printf("Size of a long int:%lu bytes(s)\n", sizeof(li));
-	printf("Size of a long long int:%lu bytes(s)\n", sizeof(ll));
-	printf("Size of a float:%lu bytes(s)\n", sizeof(f));
+	printf("Size of a long int:%lu byte(s)\n", sizeof(li));
+	printf("Size of a long long int:%lu byte(s)\n", sizeof(ll));
+	printf("Size of a float:%lu byte(s)\n", sizeof(f));
 
 	return (0);
 }
