9edf9d3a3260bf875719200b6dd07079848c7a2b
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-21 18:32:10 -0700

    order 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ *print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0(Success
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char x;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+	}
+	_putchar('\n');
+}
