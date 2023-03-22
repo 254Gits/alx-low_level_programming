e24f75b2bee244c2bb6aa6fb1217ffe9623d6e0c
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-21 19:41:45 -0700

    fix

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower- Entry point.
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	char a;
+	if(a == 'c')
+	{
+		_putchar('1');
+	}
+	else 
+	{
+		_putchar('0');
+	}
+	_putchar('\n');
+}
