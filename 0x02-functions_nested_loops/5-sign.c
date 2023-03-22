232d5d3438f811db3d6153563306acf9ecae4610
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-22 06:57:11 -0700

    plus and minus

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point.
+ *
+ *@n: variable
+ * description : check if n is greater than zero
+ *
+ * Return: 0
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		if (n <= 0)
+		{
+			_putchar ('-');
+			return (-1);
+		}
+		return (1);
+	}
+	else
+	{
+		_putchar ('0');
+		return (0);
+	}
+}
