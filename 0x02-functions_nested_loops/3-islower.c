cf9310ec1153862662e4069d954f0995956d341e
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-22 02:00:33 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point.
+ *
+ *@c : variable
+ * description : check if it is lowercase
+ *
+ * Return: int
+ *
+ */
+
+int _islower(int c)
+{
+	if (c == 'c')
+	{
+		_putchar('1');
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
