c1329fdafc4c17a99df770979ceef7b3c3a22d69
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-22 13:52:21 -0700

    last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point.
+ *
+ *@c : variable
+ * description : check last int
+ *
+ * Return: last_digit
+ *
+ */
+
+int print_last_digit(int c)
+{
+	int last_digit = c % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+		_putchar (last_digit + '0');
+	}
+	return (last_digit);
+}
