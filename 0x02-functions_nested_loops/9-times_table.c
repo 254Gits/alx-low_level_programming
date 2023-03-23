8ad7cb0765d2013097d21cdbf04c442c684af0e9
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-22 19:51:28 -0700

    tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,25 @@
 
 void times_table(void)
 {
-	int n, m;
+	int n, m, product;
 
 	for (n = 0; n <= 9; n++)
 	{
 		_putchar ('0');
 		for (m = 1; m <= 9; m++)
 		{
-			_putchar (',');
-			_putchar ('0' + (n * m) / 10);
-			_putchar ('0' + (n * m) % 10);
+			_putchar(',');
+			product = n * m;
+			if (product < 10)
+			{
+				_putchar (' ');
+				_putchar ('0' + product);
+			}
+			else
+			{
+				_putchar ('0' + product / 10);
+				_putchar ('0' + product % 10);
+			}
 		}
 		_putchar ('\n');
 	}
