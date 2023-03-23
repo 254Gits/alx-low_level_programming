62ef00ae5d6350e1bebceff1502d36aacc0725f0
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-22 19:13:09 -0700

    first of many

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point.
+ *
+ *@n: the number to begin counting at
+ * description : print all natural numbers
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 99)
+	{
+		int i = n;
+
+		while (i < 99)
+		{
+			printf("%d, ", i);
+			i++;
+		}
+	}
+	else
+	{
+		int i = n;
+
+		while (i > 99)
+		{
+			printf("%d, ", i);
+			i--;
+		}
+	}
+	printf("\n");
+}
