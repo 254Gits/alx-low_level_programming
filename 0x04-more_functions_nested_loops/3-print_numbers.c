e212316710c4c31569eaefc1b62f56e408703af8
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-24 06:31:43 -0700

    number print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point.
+ *
+ *@c: first integer
+ *
+ *
+ * Return: 1 for upper and 0 for anything else
+ *
+ */
+
+void print_numbers(void)
+{
+	int c = 0;
+
+	while ( c < '10')
+	{
+		_putchar (c);
+		c++
+	}
+	_putchar ('\n');
+}
