66fee2617af5a2d4fb0a114fd65bd06a7a6cd50a
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-24 03:02:01 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point.
+ *
+ *@c: first integer
+ *
+ *
+ * Return: 0
+ *
+ */
+
+int _isupper(int c)
+{
+	if (c == 65)
+	{
+		return (1);
+	}
+	if (c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
