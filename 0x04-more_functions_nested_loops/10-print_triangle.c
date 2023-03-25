67bab6f2e5e0a1c30fb52973bb8eb4ed803703e3
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-24 18:44:42 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle- Entry point.
+ *
+ * @size : size of triangle
+ *
+ *
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i, j, y;
+
+	if (size <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (j = size - i; j > 1; j--)
+			{
+				_putchar (32);
+			}
+			for (y = 0; y <= i; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
