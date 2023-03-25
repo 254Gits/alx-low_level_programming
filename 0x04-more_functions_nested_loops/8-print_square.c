be7a068f9d214dafee70680ef871016c287ae6b5
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-24 17:20:39 -0700

    new square shape

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square- Entry point.
+ *
+ * @size: variable for square size determinant
+ *
+ *
+ */
+
+void print_square(int size)
+{
+	int s, i;
+
+	if (size <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (s = 0; s < size; s++)
+		{
+			for (i = 0; i < size; s++)
+			_putchar (35);
+		}
+		_putchar ('\n');
+	}
+}
