5533839304021b37daa508fdb41a35e4b0940982
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-27 11:32:48 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * swap_int - swaps a and b
+  * @a: pointer to a
+  * @b:pointer to b
+  *
+  * Return: void
+  */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
