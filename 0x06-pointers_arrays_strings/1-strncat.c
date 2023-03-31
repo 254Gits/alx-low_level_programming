6e7f7cbf99e5247bdb1fcf3ad318ce57b6ba6d97
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-31 16:06:05 -0700

    strlen

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strcat - function to concatenate to the maximum value of n
+ * _strcat - function to concatenate to the maximum value of n
  * @dest: destination string
  * @src: source string
  * @n: maximum number of bytes
