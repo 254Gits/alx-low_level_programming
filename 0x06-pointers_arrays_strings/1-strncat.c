60c79b783b3a9e9521aaeda617ac998f5fec584d
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-31 14:46:45 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,9 +13,10 @@ char *_strncat(char *dest, char *src, int n)
 
 	for ( i = 0; dest [i] != '\0'; i++)
 		;
-	for (j = 0; src [j] >= n; j++)
+	for (j = 0; src [j] < n && src [j] != '\0'; j++)
 	{
-		dest [i + j] = src [j];
+		dest [i] = src [j];
+		i++;
 	}
 	dest [i + j] ='\0';
 	return (dest);
