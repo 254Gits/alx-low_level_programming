3d097e8c2049fb09b30ca0b718588a56d2f753fc
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-31 17:40:03 -0700

    copying string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,24 @@
 #include "main.h"
 
 /**
- * _strcat - function to concatenate to the maximum value of n
+ * _strncpy - function to copy string from source to dest
  * @dest: destination string
  * @src: source string
- * @n: maximum number of bytes
- * Return: pointer dest  string
+ * @n: maximum value of dtring
+ * Return: dest  string
  */
 
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
 
+	for (i = 0; i < n && src [i] != '\0'; i ++)
+	{
+		dest [i] = src [i];
+	}
+	for (;i < n; i++)
+	{
+		dest [i] = '\0';
+	}
+	return (dest);
+}
