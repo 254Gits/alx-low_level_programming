1ae8bd81cba583116416cc8c05d826fe337fce96
254Gits <emmanuelkirimi2016@gmail.com>
2023-03-31 19:11:37 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - function to concatenate to the maximum value of n
+ * @n: number of intergers
+ * @a: an array of intergers
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		int z = a[i];
+		a[i] = a[n-1-i];
+		a[n-1-i] = z;
+}
+
+}
