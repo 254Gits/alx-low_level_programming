48e32d18879a3185be7648dcf10c57cf77e6b88d
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-03 14:05:17 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * *_memcpy - copies character of pointer src to dest
+  * @src: memory area to be copied from
+  * @dest: poiner where src will be copied to
+  * @n: maximum amount of bytes
+  *
+  * Return: pointer to dest
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int y;
+
+	for (y = 0; y < n; y++)
+	{
+		src[y] = *dest;
+	}
+	return (dest);
+}
