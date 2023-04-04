b77a9d26b6c38fb686ca0ca2e2492b50cbc3a5af
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-04 06:28:12 -0700

    changing position of null

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s != '\0' && *s != c)
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
 		s++;
-		return (NULL);
 	}
-	return (0);
+	return (NULL);
 }
-
