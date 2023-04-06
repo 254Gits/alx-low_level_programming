2734102796b0a25ba865f764ff125c0a9defa737
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-06 12:07:27 -0700

    test

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_puts_recursion- entry point
+ * @s:pointer to char
+ *
+ * Return: pointer s
+ */
+
+void _puts_recursion(char *s)
+{
+	char p;
+
+	char p = *s;
+	return (*s);
+	
+}
