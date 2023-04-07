a5f4e70302e56b51e5657866a38a0ad4dbdffbbf
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-06 19:52:35 -0700

    prime last

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,33 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * is_prime_number- check the code
+ * is_prime_ - function for is_prime_number
  *
- * Return: Always 0.
+ * @n: the number to test
+ * @i: the current divisor being tested
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+
+int is_prime_(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (is_prime_(n, i - 1));
+}
+
+/**
+ * is_prime_number - checks if an digit is prime
+ *
+ * @n: the test digit
+ *
+ * Return: 1 if n is prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
-	if (n > 1 && ! n\2 )
-	{
-		return (1);
-	}
-	return (0);
+	if (n <= 1)
+		return (0);
+	return (is_prime_(n, n - 1));
 }
