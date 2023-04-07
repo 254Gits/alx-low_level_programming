cf2c9447949986f306474e8cab2cb218928c7380
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-06 19:26:14 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number- check the code
+ *
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+	if (n > 1 && ! n\2 )
+	{
+		return (1);
+	}
+	return (0);
+}
