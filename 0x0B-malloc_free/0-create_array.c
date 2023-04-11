3d985c4e7ff9fabf65314fd84cccb057e35b447f
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-11 13:14:48 -0700

    modification for checker 8

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	s = malloc((size * sizeof(char)));
-	if (s == NULL || c == 0)
+	if (s == NULL || size == 0)
 	{
 		return (NULL);
 	}
