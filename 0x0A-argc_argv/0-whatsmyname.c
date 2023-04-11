cdfd3bb4047293c2c9c63f7fc6d25744a861b456
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-10 18:39:23 -0700

    unused complilation

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  * @argv: array pointer
+  * @argc: argument count
+  *
+  * Return: 0
+  */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
