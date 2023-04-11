0e999dd30a6e234b3b562ab65685d85608434582
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-11 15:23:10 -0700

    fixed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,20 +2,33 @@
 #include <stdlib.h>
 
 /**
-  *create_array - entry point
-  * @size: int pointer
-  * @c: charcter pointer
+  *_strdup - entry point
+  *
+  * @str: charcter pointer
   *
   * Return: NULL
   */
 char *_strdup(char *str)
 {
+	unsigned int l;
+	unsigned int i;
 	char *s;
 
-	s = (char*) malloc (sizeof(char));
 	if (str == NULL)
 	{
 		return (NULL);
+	}
+
+	for (l = 0; str[l] != '\0'; l++);
+	s = malloc(l + 1);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < l; i++)
+	{
+		s[i] = str[i];
 	}
 	return (s);
 }
