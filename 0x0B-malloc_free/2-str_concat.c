43402b1d0245cf051fdb4e0009f421ad64b2d830
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-11 18:43:36 -0700

    m

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,36 +10,40 @@
   */
 char *str_concat(char *s1, char *s2)
 {
-	int len1, len2, i, j = 0;
 	char *result;
+	int i = 0, j = 0;
 
-	while (s1[len1] != '\0')
+	while (s1[i] != '\0')
 	{
-		len1++;
+		i++;
 	}
 
-	while (s2[len2] != '\0')
+	while (s2[j] != '\0')
 	{
-		len2++;
+		j++;
 	}
 
-	result = malloc(len1 + len2 + 1);
+	result = (char *)malloc(sizeof(char) * (i + j + 1));
 	if (result == NULL)
 	{
 		return (NULL);
 	}
 
+	i = 0;
 	while (s1[i] != '\0')
 	{
 		result[i] = s1[i];
 		i++;
 	}
 
+	j = 0;
 	while (s2[j] != '\0')
 	{
 		result[i + j] = s2[j];
 		j++;
 	}
+
 	result[i + j] = '\0';
 	return (result);
 }
+
