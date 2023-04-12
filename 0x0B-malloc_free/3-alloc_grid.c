9d9049ed99d97532c091409e1a509b2bada43352
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-12 14:05:43 -0700

    first run

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  **alloc_grid - entry point
+  *
+  * @width: width interger
+  * @height: height interger
+  *
+  * Return: NULL
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int **p;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(height * sizeof(int *));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < height; j++)
+	{
+		p[j] = malloc(width * sizeof(int *));
+		if (p[j] == NULL)
+		{
+			if (j >= 0)
+			{
+				free(p[j]);
+				j--;
+			}
+			free(p);
+			return (NULL);
+		}
+	}
+	return (p);
+}
