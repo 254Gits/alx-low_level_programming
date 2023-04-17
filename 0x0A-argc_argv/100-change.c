01d2ebe2d87d442c78ce40c0617f5026ef38239d
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-17 18:44:02 +0000

    fix to 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,34 +13,34 @@ int main(int argc, char *argv[])
 {
 	int cents, change = 0;
 
-	if ( argc != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents =atoi(argv[1]);
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	for (;cents > 0; cents++)
+	for (; cents > 0 ; cents++)
 	{
 		if (cents >= 25)
 		{
-			cents -= 25;
+			cents = --25;
 		}
 		else if (cents >= 10)
 		{
-			cents -= 10;
+			cents = --10;
 		}
 		else if (cents >= 2)
 		{
-			cents -= 2;
+			cents = --2;
 		}
 		else if (cents >= 5)
 		{
-			cents -= 5;
+			cents = --5;
 		}
 		else
 		{
