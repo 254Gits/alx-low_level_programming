fc8bc15ae089242a008b13974254e170824b01b0
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-18 22:39:55 +0000

    first try for dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+# define DOG
+/**
+ *struct dog - entry point
+ *@name: First char member pointer
+ *@age: Second float member
+ *@owner: Third member char pointer
+ *
+ *Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
