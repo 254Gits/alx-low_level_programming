de5d3d98415756af9745298f31f9fa7fa20d9b73
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-19 00:28:48 +0000

    doing great

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *print_dog - entry point
+ *
+ *
+ *@struct dog*g:struct
+ *@d :typedef
+ *
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+
+	if (d->name == NULL)
+	{
+		printf("(nill)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+	}
+
+}
