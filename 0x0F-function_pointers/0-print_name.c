c3eaab17babd0102b2a95015998081e8a2382dee
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-20 08:18:38 +0000

    function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ *print_name - entry point
+ *
+ *@name:character pointer
+ *@f: function pointer
+ *
+ *
+ *
+ *Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
