042111795f5e8e70c6675d6f14bbc4214189a463
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-21 15:40:53 +0000

    last last

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -7,11 +7,11 @@
  *@op: The operator
  *@f: The function associated
  */
- typedef struct op
+typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}op_t;
+} op_t;
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
