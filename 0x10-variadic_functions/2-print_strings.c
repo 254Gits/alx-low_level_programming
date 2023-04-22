327d35b673e796a0ec6cc046d151845e9ca5eff8
254Gits <emmanuelkirimi2016@gmail.com>
2023-04-22 01:30:39 +0000

    jay

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,44 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+/**
+ *print_strings - prints a name as is
+ *@n: unsigned interger
+ *@separator: character pointer
+ *
+ *Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *string;
+
+	va_list last;
+
+	va_start(last, n);
+
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(last, char *);
+		if (!separator)
+		{
+			printf("%s", string);
+		}
+		if (!string)
+		{
+			string = "(nil)";
+		}
+		if (separator && i == 0)
+		{
+			printf("%s", string);
+		}
+		else
+		{
+			printf("%s%s", separator, string);
+		}
+	}
+		printf("\n");
+
+		va_end(last);
+}
