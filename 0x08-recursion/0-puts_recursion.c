9cc8c735e454f819b4464064d504f5e47c56b059
254Gits <emmanuelkirimi2016@gmail.com>
2023-06-07 16:50:48 +0000

    recursion 1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - implementation of puts
+ *@s: pointer to character
+ *
+ *
+ *
+ *Return: newline once recursion is complete
+ */
+
+void _puts_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion (s + 1);
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
