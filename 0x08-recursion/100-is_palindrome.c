cad6823a63232e7648634095d50df4bc0e212a2d
254Gits <emmanuelkirimi2016@gmail.com>
2023-06-08 17:30:08 +0000

    sqrt recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,11 @@
+/**
+ * is_palindrome - implementation of puts
+ *@s: pointer to character
+ *
+ *
+ *
+ *Return: newline once recursion is complete
+ */
+int is_palindrome(char *s)
+{
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,39 @@
-#include "main.h"
+/**
+ *sqrt2 - function helper
+ *@z: interger
+ *@number: isecond integer
+ *
+ *
+ *
+ *Return: recursive calls
+ */
+int sqrt2(int number, int z)
+{
+	if (number <= 0 || z * z > number)
+	{
+		return (-1);
+	}
 
-/*
- *_puts_recursion - implementation of puts
+	if (z * z == number)
+	{
+		return (z);
+	}
+	else
+	{
+		return (sqrt2(number, z + 1));
+	}
+}
+
+/**
+ *_sqrt_recursion - implementation of sqrt
  *@n: pointer to character
  *
  *
  *
- *Return: -1 and square root
+ *Return: recurssive function
  */
-int _sqrt_recursion(int n){}
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 0));
+}
