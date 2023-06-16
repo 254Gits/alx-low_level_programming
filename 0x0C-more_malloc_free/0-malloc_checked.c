d4b30a99c1e0b1ed716d4d90d7efa2f3cd14ed5b
254Gits <emmanuelkirimi2016@gmail.com>
2023-06-16 10:18:11 +0000

    malloc more two

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *malloc_checked- entry point
+ *@b: placeholder
+ *
+ *
+ *
+ *Return: NULL
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
