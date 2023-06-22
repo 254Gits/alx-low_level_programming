e73cf5e1db5871b7ebd5989ad0863c36d9b3338b
254Gits <emmanuelkirimi2016@gmail.com>
2023-06-22 16:31:22 +0000

    function pointers 001

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+/**
+ *array_iterator - entry point
+ *@array: array pointer
+ *@size:size of the array
+ *@action: function pointer
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
+
