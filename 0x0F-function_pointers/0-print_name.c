3aec9af00322d3e33200847344553bc7dfd0b56b
254Gits <emmanuelkirimi2016@gmail.com>
2023-06-22 15:52:23 +0000

    function pointers 001

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 /**
- *print_name - entry point
+ *print_name - name is printed
  *@name: pointer to name
  *@f:function to pointer
  *
@@ -10,6 +10,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f !=NULL)
-	  f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
