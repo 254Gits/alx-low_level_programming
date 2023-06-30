f671f0a6af15b97dea9daaf77207e969cdce9f20
254Gits <emmanuelkirimi2016@gmail.com>
2023-06-30 22:51:38 +0000

    betty fixed

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,18 +2,18 @@
 #define HJ_K
 
 /**
- *  * struct list_s - singly linked list
- *   * @str: string - (malloc'ed string)
- *    * @len: length of the string
- *     * @next: points to the next node
- *      *
- *       * Description: singly linked list node structure
- *        */
+ *struct list_s - singly linked list
+ *@str: string - (malloc'ed string)
+ *@len: length of the string
+ *@next: points to the next node
+ *
+ *Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-        struct list_s *next;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
