131135b5b442f72c8bac5eb4e9e7fcb8ed6933a7
254Gits <emmanuelkirimi2016@gmail.com>
2023-07-05 21:28:44 +0000

    fourth double

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef
+#define HUY_u
 /**
  *struct listint_s - singly linked list
  *@n: integer
@@ -14,3 +16,4 @@ typedef struct listint_s
 	struct listint_s *next;
 }listint_t;
 size_t print_listint(const listint_t *h);
+#endif
