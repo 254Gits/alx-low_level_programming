1c6869ba28c4899e1d8a9b1ca77a65e110b19880
254Gits <emmanuelkirimi2016@gmail.com>
2023-07-11 21:57:46 +0000

    bit 5

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *clear_bit - entry point
+ *@n: long interger
+ *@index: starting point
+ *
+ *
+ *Return : 1 on success and -1 when it fails
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+
+	*n &= -(1UL << index);
+	return (1);
+}
