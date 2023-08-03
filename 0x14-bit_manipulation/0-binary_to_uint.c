afc9e15ef517413f163e670f7368b14fb7c95e3f
254Gits <emmanuelkirimi2016@gmail.com>
2023-08-03 21:18:45 +0000

    take one

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * check_validity - check for string
+ * @b: character
+ *
+ * Return : 1
+ */
+
+int check_validity(const char *b)
+{
+	if (b == NULL)
+		return (0);
+
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+		b++;
+	}
+	return (1);
+}
+
+/**
+ * binary_to_uint - binary holder
+ * @b: pointer
+ *
+ * Return: Decimal
+ */
+
+unsigned int binary_to_unit(const char *b)
+{
+	unsigned int d = 0;
+
+	if(!check_validity(b))
+	{
+		return (0);
+	}
+
+	while (*b != '\0')
+	{
+		d = d << 1;
+		d += (*b - '0');
+		b++;
+	}
+	return (d);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stddef.h>
+unsigned int binary_to_uint(const char *b);
+int check_validity(const char *b);
+#endif
