3b58f25e70a91a410e53b9e38c55620405599428
254Gits <emmanuelkirimi2016@gmail.com>
2023-08-03 22:03:57 +0000

    check binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,16 @@
+#include "main.h"
+/**
+ * print_binary- place holder
+ * @n: long interger
+ *
+ * Return: void
+ */
+
 void print_binary(unsigned long int n)
 {
-	return ();
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+		_putchar((n & 1) + '0');
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,8 @@
 #ifndef _MAIN_L
 #define _MAIN_L
 
-
+int _putchar(char c);
+void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int check_validity(const char *b);
 #include <stddef.h>
