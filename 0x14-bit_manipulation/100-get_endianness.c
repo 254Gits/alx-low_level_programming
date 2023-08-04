ff579a6947630c4e9f86f5e0f1ef5241c84c91bc
254Gits <emmanuelkirimi2016@gmail.com>
2023-08-04 19:38:17 +0000

    endian test

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,17 @@
+/**
+ * get_endianness - determines little edian
+ *
+ * Return: 1 if big edian and 1 if little
+ */
 int get_endianness(void)
 {
-	    unsigned int test = 1;
-	        unsigned char *endian = (unsigned char *)&test;
+	unsigned int test = 1;
+	unsigned char *endian = (unsigned char *)&test;
 
-		    if (*endian == 1)
-			            return 1; // Little-endian
-		        else
-				        return 0; // Big-endian
+
+	if (*endian == 1)
+		return (1);
+
+	else
+		return (0);
 }
-
