56868d020c53c821a497c1696b89327ca805a68b
254Gits <emmanuelkirimi2016@gmail.com>
2023-08-17 17:47:48 +0000

    c again

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints a double linked list
+ * @h: pointer to list
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t ndes = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+		ndes++;
+	}
+	return (ndes);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+## README.md for double list ##
