d7dce4df0b82186148bc698fff7b91c2b949c233
254Gits <emmanuelkirimi2016@gmail.com>
2023-08-17 22:43:15 +0000

    betty fix

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -10,10 +10,10 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
-	for (; head !=NULL; head = head->next)
+	for (; head != NULL; head = head->next)
 	{
 		sum += head->n;
 	}
 
-	return sum;
+	return (sum);
 }
